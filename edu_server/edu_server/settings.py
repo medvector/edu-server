ba6ee3e4f52448d3ed664268086eedbb4f2e928f
kirkutirev <kutirev-k@mail.ru>
2018-08-08 15:08:10 +0300

    make string comparison in util case-insensitive

--- edu_server/edu_server/settings.py
+++ edu_server/edu_server/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import psycopg2
-from edu_server.secret_settings import SECRET_KEY
+from .secret_settings import SECRET_KEY
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
